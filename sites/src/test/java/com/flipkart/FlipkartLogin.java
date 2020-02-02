9f0d4d3bd4087fa45d8c080f8a4c016f354cd75d
GowthamKandanuru <50964789+GowthamKandanuru@users.noreply.github.com>
2020-02-02 17:45:47 +0530

    Add files via upload

---
+++ sites/src/test/java/com/flipkart/FlipkartLogin.java
@@ -0,0 +1,18 @@
+package com.flipkart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+
+public class FlipkartLogin {
+
+	WebDriver driver;
+
+	@Test
+	public void init() {
+		System.setProperty("webdriver.gecko.driver","K:/EclipseProjects/Workplace/First/drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+}
