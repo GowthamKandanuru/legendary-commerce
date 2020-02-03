04fed9dd47669955c34d83004661648b98b58385
gowtham kandanuru <you@example.com>
2020-02-04 01:57:50 +0530

    added username field

--- sites/src/test/java/com/flipkart/FlipkartLogin.java
+++ sites/src/test/java/com/flipkart/FlipkartLogin.java
@@ -1,5 +1,6 @@
 package com.flipkart;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
@@ -14,5 +15,7 @@ public class FlipkartLogin {
 		System.setProperty("webdriver.gecko.driver","K:/EclipseProjects/Workplace/First/drivers/geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.get("https://www.flipkart.com");
+		driver.findElement(By.xpath("//input[@type='text'][@class='_2zrpKA _1dBPDZ']")).sendKeys("8332056542");
 	}
+	
 }
